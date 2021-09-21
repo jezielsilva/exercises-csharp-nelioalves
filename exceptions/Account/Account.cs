25597403b6e62f72156940a36edb2dc424e00783
Jeziel Silva <silvajeziel22@gmail.com>
2021-09-21 11:51:08 -0300

    Exercicios sobre trycatch.

---
+++ exceptions/Account/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using exceptions.Exceptions;
+
+namespace exceptions.Account
+{
+    class Account : ApplicationException
+    {
+        public int number { get; set; }
+        public string holder { get; set; }
+        public double balance { get; set; }
+        public double withdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            this.number = number;
+            this.holder = holder;
+            this.balance = balance;
+            this.withdrawLimit = withdrawLimit;
+        }
+
+        public Account()
+        {
+
+        }
+
+        public void deposit(double amount)
+        {
+            balance += amount;
+        }
+
+        public void withdraw(double amount)
+        {
+            if(amount > withdrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit.");
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine();
+                Console.WriteLine($"New Balance: {balance.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
+        }
+    }
+}
---
+++ exceptions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using exceptions.Exceptions;
+namespace exceptions.Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data ");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine());
+                Console.Write("Withdraw limit: ");
+                double withdraw = double.Parse(Console.ReadLine());
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine());
+                Account account = new Account(number, holder, balance, withdraw);
+                account.withdraw(amount);
+                
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+        }
+    }
+}
